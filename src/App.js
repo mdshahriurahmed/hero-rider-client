9b01240f20a17449d61e4f3a3de92a76fa7b8436
mdshahriurahmed <shahriurahmedcse@gmail.com>
2023-03-21 22:32:02 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,22 +2,31 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "../../Styles/CommonStyle.css"
+// logo import
 import logo from '../../Images/logo.png'
+
+// css files import
+import "../../Styles/CommonStyle.css"
+import "../../Styles/HeaderStyle.css"
+// fontawesome icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus, faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
 
         <Navbar className='bg-custom1 !text-white' variant="dark">
             <Container>
-                <Navbar.Brand href="#home" className='navBrand'>
+                <Link to="/" className='navBrand'>
                     <div className='h-full px-4 py-3 bg-white logoClass'><img src={logo} alt="" /></div>
 
-                </Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Link>
+                <Nav className="ms-auto">
+                    <Link to='/' className='homeMenu'>Home </Link>
+                    <Link to='/signup' className='SignupBtn px-3 fw-bold'><span className='signUpLogIn'>Sign Up</span> <FontAwesomeIcon icon={faUserPlus} /></Link>
+                    <Link to="/login" className='loginBtn px-3 fw-bold'><span className='signUpLogIn'>Login</span> <FontAwesomeIcon icon={faRightToBracket} /></Link>
+
                 </Nav>
             </Container>
         </Navbar>
